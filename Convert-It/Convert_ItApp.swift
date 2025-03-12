174d42f7f330f420f1980cadf1802aae051951cd
Arda Kaan Aydilek <ardakaanaydilek@gmail.com>
2025-03-12 14:45:55 +0300

    Initial Commit

---
+++ Convert-It/Convert_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Convert_ItApp.swift
+//  Convert-It
+//
+//  Created by arda on 12.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Convert_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
